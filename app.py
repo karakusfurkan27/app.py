4a94e9ed982054902f25cbf57a044830cf3397d8
Furkan Karakuş <79601715+karakusfurkan27@users.noreply.github.com>
2024-12-20 16:17:24 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+name = input('What is your name?')
+print('Hi', name)
+
+sampleInteger = 123123123123123123123123123123123123
+print(sampleInteger)
+print(type(sampleInteger))
+
+sampleBinary = 0B1010
+sampleBinary = 0b1010
+
+print(sampleBinary)
+print(type(sampleBinary))
+
+sampleOctave = 0o543
+
+print(sampleOctave)
+print(type(sampleOctave))
+
+sampleHexadecimal = 0X2F3
+
+print(sampleHexadecimal)
+print(type(sampleHexadecimal))
+
+sampleFloat = 4.2
+
+
+print(sampleFloat)
+print(type(sampleFloat))
+
+sampleBigFloat = 1.8E23
+
+print(sampleBigFloat)
+print(type(sampleBigFloat))
+
+sampleSmallFloat = 2.3E-54
+
+print(sampleSmallFloat)
+print(type(sampleSmallFloat))
+00000000000000000000000000000000000000000000000000000
+# 53 tane 0 olucak
+
+sampleString = "This is \"stupid\" exsample for printing"
+
+sampleString += '\ \x61\', \'\xAE\' and \'\uA7B5\' characters'
+
+sampleString += "\t becasue it is for presenting escape sequence"
+print(sampleString)
